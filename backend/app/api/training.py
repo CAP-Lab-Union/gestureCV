7de48e3b0cf1d9cab6d5f417b30f2ada5905db27
james <jameslin809@gmail.com>
2025-02-27 02:38:46 -0500

    Feat: Train improvements
    Fix

--- backend/app/api/training.py
+++ backend/app/api/training.py
@@ -1,53 +1,8 @@
-# from fastapi import APIRouter, BackgroundTasks
-# from pydantic import BaseModel
-# from ..services.training import train_model
-
-# router = APIRouter()
-
-# class TrainingPayload(BaseModel):
-#      # list of base64 encoded images
-#      # the label for these images
-#     training_images: list[str]  
-#     gesture_label: int         
-
-# @router.post("/train")
-# async def train_endpoint(payload: TrainingPayload, background_tasks: BackgroundTasks):
-#     background_tasks.add_task(train_model, payload.training_images, payload.gesture_label)
-#     return {"status": "Training started"}
-
-# from pyexpat import model
-# from typing import List
-# from fastapi import APIRouter, BackgroundTasks, HTTPException
-# from flask import app
-# from pydantic import BaseModel
-# from ..services.training import train_model, train_model_real_time 
-
-# router = APIRouter()
-
-# class TrainingPayload(BaseModel):
-#     training_images: list[str]  # list of base64 encoded images
-#     gesture_name: str           # the gesture name provided by the frontend
-
-
-# class TrainModelRequest(BaseModel):
-#     training_images: List[str]
-#     gesture_name: str
-#     learning_rate: float = 0.001
-#     epochs: int = 5
-#     batch_size: int = 1
-
-
-# @router.post("/train")
-# async def train_endpoint(payload: TrainingPayload, background_tasks: BackgroundTasks):
-#     background_tasks.add_task(train_model, payload.training_images, payload.gesture_name)
-#     return {"status": "Training started"}
-
-
 
 from typing import List
 from fastapi import APIRouter, BackgroundTasks, HTTPException
 from pydantic import BaseModel
-from ..services.training import train_model_real_time  # Adjust path as needed
+from ..services.training import train_model_real_time  
 
 router = APIRouter(prefix="/gesture", tags=["gesture_training"])
 
